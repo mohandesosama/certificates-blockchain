8bae7c405ac3e8e71e85762126aadfd9d7a67e8f
unknown <mohandesosama@yahoo.com>
2022-04-01 18:12:05 +0400

    add block form

--- BlockchainController.js
+++ BlockchainController.js
@@ -1,18 +1,46 @@
+var express = require('express');
 const path=require("path");
-const express=require('express');
-const router = express.Router();
+const body_parser=require('body-parser');
+var multer = require('multer');
+var upload = multer();
 class BlockchainController{
     constructor(app){
         this.app=app;
-        this.helloWorld();
-        this.app.use('/',router)
+        //GET forms here
+        this.homePage();
+        this.addBlockPage();
+
+        // for parsing application/json
+        this.app.use(body_parser.json()); 
+        // for parsing application/xwww-
+        this.app.use(body_parser.urlencoded({ extended: true })); 
+        //form-urlencoded
+        this.app.use(upload.array()); 
+
+        //POST forms here
+        this.addBlock();
     }
-    helloWorld(){
-        router.get("/", function (req, res) {
+    homePage(){
+        this.app.get("/", function (req, res) {
                 //res.sendFile(path.resolve( __dirname, 'www/index.html' ));
                 res.sendFile(path.join(__dirname, '/index.html'));
                 //res.end();
             });
+    }
+    addBlockPage(){
+        this.app.get("/add", function(req, res){
+                res.sendFile(path.join(__dirname,'/addBlock.html'));
+        });
+    }
+    addBlock(){
+        this.app.post("/add", function(req,res){
+            
+            //return console.log("New Block has been added into the blockchain= "+req.body.block);
+            var val="New Block has been added into the blockchain= "+req.body.block;
+            //.send(val);
+            //return res.status(400).json({ added: true, val });
+            res.send(val)
+        });
     }
 }
 
--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 
 ## References
 * [Solving the port problem"(https://medium.com/@oaeskuruni27/3-tricks-for-solving-the-heroku-application-error-adeef5782b9d)
+* [Sending HTML file](https://codeforgeek.com/render-html-file-expressjs/)
