d876b915ee46c96f592615ae19efa495aded0f16
unknown <mohandesosama@yahoo.com>
2022-04-01 05:04:24 +0400

    updated use files.

--- BlockchainController.js
+++ BlockchainController.js
@@ -2,14 +2,16 @@ class BlockchainController{
     constructor(app){
         this.app=app;
         this.helloWorld();
+        this.app.use( '/www', express.static( path.resolve( __dirname, 'www' ) ) );
     }
     helloWorld(){
-        this.app.get("/", async function (req, res) {
-                res.sendFile("index.html");
+        this.app.get("/", function (req, res) {
+                res.sendFile(path.resolve( __dirname, 'www/index.html' ));
                 res.end();
             });
     }
 }
+
 module.exports = (app)=> {
     return new BlockchainController(app);
 };
