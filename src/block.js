80697007c683828452264c8867a48df270af39b6
unknown <mohandesosama@yahoo.com>
2022-04-23 16:10:20 +0400

    added the validate block functionality

--- src/block.js
+++ src/block.js
@@ -1,3 +1,5 @@
+const { SHA256 } = require("crypto-js");
+
 class Block{
     constructor(data){
         this.height = null;
@@ -5,6 +7,21 @@ class Block{
         this.time = null;
         this.previousBlockHash = null;
         this.hash=null;
+    }
+    validate()
+    {
+        self=this;
+        return new Promise(async (resolve,reject)=>{
+            let current_hash=self.hash;
+            self.hash=null
+            let calc_hash=SHA256(JSON.stringify(self)).toString();
+            self.hash=current_hash;
+            if(calc_hash == current_hash)
+            {
+                resolve(true)
+            }
+            reject(false)
+        })
     }
 }
 module.exports.Block= Block;
