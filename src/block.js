0d86b405ad8d3f368ee554d7b55cca174584a5a4
unknown <mohandesosama@yahoo.com>
2022-04-24 04:54:35 +0400

    valid implementation

--- src/block.js
+++ src/block.js
@@ -10,17 +10,23 @@ class Block{
     }
     validate()
     {
-        self=this;
+        let self=this;
         return new Promise(async (resolve,reject)=>{
+            // if not genesis block.
             let current_hash=self.hash;
+            console.log('current hash: ' + current_hash)
             self.hash=null
             let calc_hash=SHA256(JSON.stringify(self)).toString();
+            console.log("calc hash: " + calc_hash)
             self.hash=current_hash;
             if(calc_hash == current_hash)
             {
                 resolve(true)
             }
-            reject(false)
+            else{
+                resolve(false)
+            }   
+
         })
     }
 }
